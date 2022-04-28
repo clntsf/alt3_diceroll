2d035cde31ff316af9b0e340460de5c1a35a2d26
ctsf1 <colin.tsf@gmail.com>
2022-04-28 12:26:07 +0100

    dicerolls now done by numpy

--- src/diceroller_1.py
+++ src/diceroller_1.py
@@ -1,14 +1,19 @@
-from numpy.random import randint
+from random import randint
 from collections import Counter
 from time import sleep, time
 
 def display_welcome_msg() -> None:
     print("\nWelcome to the diceroll program! here you can choose a number of 6-sided dice to roll, and how many times to roll.")
-    print("The program will then sum each of these rolls and display the probability of the possible sums occurring by number")
+    print("The program will then sum each of these rolls and display the probability of the possible sums occurring by number\n")
+
 
 def get_diceroll_counter(num_rolls: int, num_dice: int):
-    rolls = randint(1,7,size=(num_dice,num_rolls))
-    return Counter(sum(rolls))
+    sums = [ sum([
+        randint(1,6) for d in range(num_dice)
+        ]) for r in range(num_rolls)
+    ]
+    return Counter(sums)
+
 
 def display_probabilities(disp_by_prob: bool, counter: Counter, num_rolls: int):
 
@@ -17,35 +22,28 @@ def display_probabilities(disp_by_prob: bool, counter: Counter, num_rolls: int):
     else:                               # sort by first item (result number)
         prob = sorted(counter.items(), key = lambda n:n[0])
 
-    # formatting the output
-    lj1 = len(str( max(counter.keys()) )) + 7
-    lj2 = len(str( max(counter.values()) )) + 6
-
     print(f"\n\t\tResults: \n{'-'*40}\n")
     for (k,v) in prob:
-        cols = [
-            f"Sum of {k}".ljust(lj1),
-            f"{v} rolls".ljust(lj2),
-            f"({ round(100*v/num_rolls,3) }% chance)"
-        ]
-        print(" | ".join(cols))
+        print(f"Sum of {k}: {v} rolls ({round(100*v/num_rolls,3)}% chance)")
+
 
 def main():
 
     display_welcome_msg()                                           # printing welcome message
-    sleep(0.3)
+    sleep(0.5)
 
-    num_dice = int(input("\nNumber of dice to roll: ")); sleep(0.2)   # getting user inputs
+    num_dice = int(input("Number of dice to roll: ")); sleep(0.2)   # getting user inputs
     num_rolls = int(input("Number of dice-rolls: ")); sleep(0.2)
-    disp_by_prob = ( input("Display possible sums by decreasing probability (y) or by result number (n): ").lower() == "y" )
+    disp_by_prob = ( input("Display possible sums by decreasing probability? (y/n): ").lower() == "y" )
 
     t0 = time()
     sum_counter = get_diceroll_counter(num_rolls, num_dice)         # getting the counter
 
     el = time()-t0          # shortening sleep by the runtime of get_diceroll_counter() to standardize wait time
-    sleep( max(0, 0.3-el) )
+    sleep( max(0, 0.5-el) )
 
     display_probabilities(disp_by_prob, sum_counter, num_rolls)     # print the probability display readout
+
 
 if __name__ == "__main__":
     main()
